29a7026e714f70ad113ee5debbce522edf253fd3
Chethan P S <165404902+chethan313@users.noreply.github.com>
2024-04-02 19:00:04 +0530

    Create Radius.c
    "C PROGRAM TO READ RADIUS AND FIND THE AREA AND VOLUME OF SPHERE">

---
+++ Radius.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+#define PI 3.142
+void main()
+{
+  float r,area,volume;
+clrsctr();
+printf("Enter the volume of radius:");
+scanf("%f",&r);
+area=4.0*PI*r*r;
+volume=(4.0/3.0)*PI*r*r*r;
+printf("\n Area of sphere=%3f",area);
+printf("\n volume of sphere=%3f",volume);
+getch();
+}
