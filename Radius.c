469fb97bcf0ec0da967b79ac6b223266d84f0c16
Chethan P S <165404902+chethan313@users.noreply.github.com>
2024-07-22 05:51:46 +0530

    Update Radius.c

--- Radius.c
+++ Radius.c
@@ -3,6 +3,7 @@
 #define PI 3.142
 void main()
 {
+  
   float r, area, volume;
 clrsctr();
 printf("Enter the volume of radius  :");
@@ -12,4 +13,5 @@ volume=(4.0/3.0)*PI*r*r*r;
 printf("\n Area of sphere=%3f",area);
 printf("\n volume of sphere=%3f",volume);
 getch();
+  
 }
